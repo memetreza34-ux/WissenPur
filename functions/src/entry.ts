1049c4a013f1c4762c2069173a5d7ea4d1c38dd3
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 15:54:55 +0200

    export account privacy functions

--- functions/src/entry.ts
+++ functions/src/entry.ts
@@ -2,3 +2,4 @@ export * from './index.js';
 export { recordRoundResult } from './legacyRound.js';
 export { startSecureRankedQuiz } from './secureStart.js';
 export { revealSecureRankedQuiz } from './secureReveal.js';
+export { exportMyData, deleteMyAccount } from './account.js';
