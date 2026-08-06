dc463f401b83726beaa061d1ccd32774aafcd1a0
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 16:18:44 +0200

    type legal release environment variables

--- wissenpur/src/vite-env.d.ts
+++ wissenpur/src/vite-env.d.ts
@@ -5,6 +5,16 @@ interface ImportMetaEnv {
   readonly VITE_ENABLE_APPCHECK_DEBUG?: 'true' | 'false';
   readonly VITE_USE_FUNCTIONS_EMULATOR?: 'true' | 'false';
   readonly VITE_FIRESTORE_DATABASE_ID?: string;
+  readonly VITE_PUBLIC_APP_URL?: string;
+  readonly VITE_LEGAL_OPERATOR_NAME?: string;
+  readonly VITE_LEGAL_STREET?: string;
+  readonly VITE_LEGAL_POSTAL_CITY?: string;
+  readonly VITE_LEGAL_COUNTRY?: string;
+  readonly VITE_LEGAL_EMAIL?: string;
+  readonly VITE_PRIVACY_EMAIL?: string;
+  readonly VITE_SUPPORT_EMAIL?: string;
+  readonly VITE_LEGAL_EFFECTIVE_DATE?: string;
+  readonly VITE_MINIMUM_AGE?: string;
 }
 
 interface ImportMeta {
