e104af94a9ac3445bce97c7a2a9dcfe7d5555f94
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 15:57:39 +0200

    mount account privacy controls

--- wissenpur/src/main.tsx
+++ wissenpur/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { AccountPrivacyPanel } from './components/AccountPrivacyPanel';
 import ReleaseApp from './ReleaseApp';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReleaseApp />
+    <AccountPrivacyPanel />
   </StrictMode>,
 );
