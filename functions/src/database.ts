92d4d4cf86956390147b7e8e93d3d18f24611f7c
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 17:12:58 +0200

    bootstrap shared cloud function runtime options

--- functions/src/database.ts
+++ functions/src/database.ts
@@ -1,5 +1,13 @@
 import { getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { setGlobalOptions } from 'firebase-functions/v2';
+
+setGlobalOptions({
+  region: 'europe-west1',
+  memory: '256MiB',
+  timeoutSeconds: 30,
+  maxInstances: 10,
+});
 
 export const firebaseApp = getApps()[0] ?? initializeApp();
 
