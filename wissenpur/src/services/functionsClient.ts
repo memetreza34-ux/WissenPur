7d1f5dbe356f8ed8a9ae29ed4fd8d9eec352f4fc
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 15:55:25 +0200

    add shared firebase functions client

---
+++ wissenpur/src/services/functionsClient.ts
@@ -0,0 +1,17 @@
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
+import { app } from '../firebase';
+
+export const functions = getFunctions(app, 'europe-west1');
+
+const emulatorState = globalThis as typeof globalThis & {
+  __WISSENPUR_SHARED_FUNCTIONS_EMULATOR_CONNECTED__?: boolean;
+};
+
+if (
+  import.meta.env.DEV &&
+  import.meta.env.VITE_USE_FUNCTIONS_EMULATOR === 'true' &&
+  !emulatorState.__WISSENPUR_SHARED_FUNCTIONS_EMULATOR_CONNECTED__
+) {
+  connectFunctionsEmulator(functions, '127.0.0.1', 5001);
+  emulatorState.__WISSENPUR_SHARED_FUNCTIONS_EMULATOR_CONNECTED__ = true;
+}
