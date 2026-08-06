611bc088d90dd8533dde39e0307e87c66bac5dd2
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 16:21:27 +0200

    mount public legal information panel

--- wissenpur/src/main.tsx
+++ wissenpur/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AccountPrivacyPanel } from './components/AccountPrivacyPanel';
 import { LearningPlanPanel } from './components/LearningPlanPanel';
+import { LegalPanel } from './components/LegalPanel';
 import ReleaseApp from './ReleaseApp';
 import './index.css';
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReleaseApp />
     <LearningPlanPanel />
+    <LegalPanel />
     <AccountPrivacyPanel />
   </StrictMode>,
 );
