2ba5c5cd4ddd3f75336e23642d9b76815e717bf0
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 19:49:45 +0200

    deploy only the strict ranked submission callable

--- functions/src/entry.ts
+++ functions/src/entry.ts
@@ -1,10 +1,10 @@
 export {
-  submitRankedQuiz,
   claimDailyQuestReward,
   spinDailyWheel,
   purchaseShopItem,
   consumePowerUp,
 } from './economyCallables.js';
+export { submitRankedQuiz } from './secureSubmit.js';
 export { startSecureRankedQuiz } from './secureStart.js';
 export { revealSecureRankedQuiz } from './secureReveal.js';
 export { exportMyData, deleteMyAccount } from './account.js';
