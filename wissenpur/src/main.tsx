f100a9c68968cc646d68fab4ce30d179087761e3
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 16:07:12 +0200

    mount adaptive learning plan controls

--- wissenpur/src/main.tsx
+++ wissenpur/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AccountPrivacyPanel } from './components/AccountPrivacyPanel';
+import { LearningPlanPanel } from './components/LearningPlanPanel';
 import ReleaseApp from './ReleaseApp';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReleaseApp />
+    <LearningPlanPanel />
     <AccountPrivacyPanel />
   </StrictMode>,
 );
