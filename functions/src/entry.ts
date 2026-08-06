8e98a71e217b4347ca6a8ce0f58fc21e56f0e5b6
memetreza34-ux <memetreza34@gmail.com>
2026-08-06 17:13:13 +0200

    deploy only secure modular callables

--- functions/src/entry.ts
+++ functions/src/entry.ts
@@ -1,5 +1,10 @@
-export * from './index.js';
-export { recordRoundResult } from './legacyRound.js';
+export {
+  submitRankedQuiz,
+  claimDailyQuestReward,
+  spinDailyWheel,
+  purchaseShopItem,
+  consumePowerUp,
+} from './economyCallables.js';
 export { startSecureRankedQuiz } from './secureStart.js';
 export { revealSecureRankedQuiz } from './secureReveal.js';
 export { exportMyData, deleteMyAccount } from './account.js';
